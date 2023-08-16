d82a6b8d122962309137c09e316421a5ba3dcce8
Evgenii Boiko <jax7777@yandex.ru>
2023-08-16 23:45:46 +0300

    Add modified README.md and some logic

--- src/main/java/ru/otus/java/basic/lesson14/MainApp.java
+++ src/main/java/ru/otus/java/basic/lesson14/MainApp.java
@@ -1,4 +1,24 @@
 package ru.otus.java.basic.lesson14;
 
 public class MainApp {
+    public static void main(String[] args) {
+        String[][] arr = new String[4][4];
+        arr.toString();
+        System.out.println(arr.toString());
+
+        castAndSumElements(arr);
+    }
+
+    public static int castAndSumElements(String[][] arr) {
+
+        for (String[] i: arr) {
+            for (String j: i) {
+
+            }
+
+        }
+
+        System.out.println();
+        return -1;
+    };
 }
