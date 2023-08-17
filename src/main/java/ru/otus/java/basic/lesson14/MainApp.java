3a55a926ad65bb9fd9332c67eafa64cf4e270f6e
Evgenii Boiko <jax7777@yandex.ru>
2023-08-18 02:51:28 +0300

    Add lesson 14 full logic

--- src/main/java/ru/otus/java/basic/lesson14/MainApp.java
+++ src/main/java/ru/otus/java/basic/lesson14/MainApp.java
@@ -1,24 +1,58 @@
 package ru.otus.java.basic.lesson14;
 
+import ru.otus.java.basic.lesson14.createdexceptions.AppArrayDataException;
+import ru.otus.java.basic.lesson14.createdexceptions.AppArraySizeException;
+
 public class MainApp {
     public static void main(String[] args) {
-        String[][] arr = new String[4][4];
-        arr.toString();
-        System.out.println(arr.toString());
-
-        castAndSumElements(arr);
+        String[][] arr = {{"0", "0", "5", "Etgdfgdfg"},
+                {"6", "0", "0", "0"},
+                {"0", "4", "0", "10"},
+                {"10", "0", "0", "0"}};
+        printArray(arr);
+        try {
+            castAndSumElements(arr);
+        } catch (AppArraySizeException e) {
+            System.out.println("Failed to sum array elements: dimension/s is/are greater than 4!");
+            System.out.println("error: " + e.toString());
+            e.printStackTrace();
+        }
     }
 
-    public static int castAndSumElements(String[][] arr) {
-
-        for (String[] i: arr) {
-            for (String j: i) {
-
-            }
-
+    private static void castAndSumElements(String[][] arr) throws AppArraySizeException {
+        int sum = 0;
+        int interimSum;
+        if (arr.length != 4 || arr[0].length != 4) {
+            throw new AppArraySizeException();
         }
+        System.out.println("\nDimensions check succeeded!\n");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                try {
+                    sum += Integer.parseInt(arr[i][j]);
+                    interimSum = sum;
+                    System.out.println("Interim sum equals: " + interimSum);
+                } catch (NumberFormatException ex) {
+                    AppArrayDataException appArrayDataException = new AppArrayDataException("custom AppArrayDataException", ex);
+                    System.out.println("\nFailed to parse String element to int!\n" +
+                            "The element is: " + arr[i][j] + "\n" +
+                            "Its indexes are: [" + i + "][" + j + "]\n");
+                    appArrayDataException.printStackTrace();
+                    return;
+                }
+            }
+        }
+        System.out.println("================================");
+        System.out.println("Success! Array elements' sum equals " + sum);
+    }
 
-        System.out.println();
-        return -1;
-    };
+    private static void printArray(String[][] arr) {
+        System.out.println("Array is :::::::::::::\n");
+        for (String[] row : arr) {
+            for (String element : row) {
+                System.out.print(element + "   ");
+            }
+            System.out.println();
+        }
+    }
 }
---
+++ src/main/java/ru/otus/java/basic/lesson14/createdexceptions/AppArraySizeException.java
@@ -0,0 +1,4 @@
+package ru.otus.java.basic.lesson14.createdexceptions;
+
+public class AppArraySizeException extends Exception{
+}
