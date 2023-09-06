a5de87351a7c2c160103c982a2c35c1d8bf2310d
Evgenii Boiko <jax7777@yandex.ru>
2023-09-06 23:36:01 +0300

    Lesson 14: resolve issues after review

--- src/main/java/ru/otus/java/basic/lesson14/MainApp.java
+++ src/main/java/ru/otus/java/basic/lesson14/MainApp.java
@@ -3,9 +3,11 @@ package ru.otus.java.basic.lesson14;
 import ru.otus.java.basic.lesson14.createdexceptions.AppArrayDataException;
 import ru.otus.java.basic.lesson14.createdexceptions.AppArraySizeException;
 
+import java.util.Arrays;
+
 public class MainApp {
     public static void main(String[] args) {
-        String[][] arr = {{"0", "0", "5", "Etgdfgdfg"},
+        String[][] arr = {{"0", "0", "5", "прапр"},
                 {"6", "0", "0", "0"},
                 {"0", "4", "0", "10"},
                 {"10", "0", "0", "0"}};
@@ -13,7 +15,7 @@ public class MainApp {
         try {
             castAndSumElements(arr);
         } catch (AppArraySizeException e) {
-            System.out.println("Failed to sum array elements: dimension/s is/are greater than 4!");
+            System.out.println("Failed to sum array elements: dimension/s does/do not equal 4!");
             System.out.println("error: " + e.toString());
             e.printStackTrace();
         }
@@ -22,10 +24,11 @@ public class MainApp {
     private static void castAndSumElements(String[][] arr) throws AppArraySizeException {
         int sum = 0;
         int interimSum;
-        if (arr.length != 4 || arr[0].length != 4) {
-            throw new AppArraySizeException();
+        for (int k = 0; k < 4; k++) {
+            if (arr.length != 4 || arr[k].length != 4) {
+                throw new AppArraySizeException();
+            }
         }
-        System.out.println("\nDimensions check succeeded!\n");
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
                 try {
@@ -33,12 +36,7 @@ public class MainApp {
                     interimSum = sum;
                     System.out.println("Interim sum equals: " + interimSum);
                 } catch (NumberFormatException ex) {
-                    AppArrayDataException appArrayDataException = new AppArrayDataException("custom AppArrayDataException", ex);
-                    System.out.println("\nFailed to parse String element to int!\n" +
-                            "The element is: " + arr[i][j] + "\n" +
-                            "Its indexes are: [" + i + "][" + j + "]\n");
-                    appArrayDataException.printStackTrace();
-                    return;
+                    throw new AppArrayDataException("custom AppArrayDataException", ex);
                 }
             }
         }
@@ -54,5 +52,7 @@ public class MainApp {
             }
             System.out.println();
         }
+//        // или вывод таким способом:
+//        System.out.println(Arrays.toString(arr));
     }
 }
